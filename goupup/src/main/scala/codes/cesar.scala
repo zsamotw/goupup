1c168047539fce09fb67430ac6fbd5a286dc93b8
tw <donkemot@interia.pl>
2018-01-05 11:35:36 +0100

    20180105

--- goupup/src/main/scala/codes/cesar.scala
+++ goupup/src/main/scala/codes/cesar.scala
@@ -1,12 +1,33 @@
 package cesar
 
-class Cesar(val fraze: String, val shift: Int) {
-  def encode = {
-    val ll = fraze map(el => el.toInt + shift)
+class Cesar(val shift: Int) {
+
+  val maxBound: Int = 127
+  val minBound: Int = 32
+
+  def encode(fraze: String) = {
+    val ll = fraze map(el => setCode(el, shift))
     ll.toList.foldLeft("")((acc,el) => acc + el.toChar)
   }
   def decode(str: String) = {
-    val ll = str map(el => ( el.toInt - shift ).toChar)
+    val ll = str map(el => getCharCode(el, shift))
     ll.toList.foldLeft("")((acc, el) => acc + (el.toInt).toChar)
+  }
+
+  def setCode(ch: Char, shift: Int): Int = {
+    val n = ch.toInt + shift
+    n match {
+      case _ if(n > maxBound) => n % maxBound + minBound - 1
+      case _ => n
+    }
+  }
+
+  def getCharCode(ch: Char, shift: Int): Int = {
+    val n = ch.toInt - shift
+    n match {
+      case _ if(n < 0) => maxBound - ( (Math.abs(n) + minBound) % maxBound ) + 1
+      case _ if(n < minBound) => maxBound - (minBound - n) + 1 
+      case _ => n
+    }
   }
 }
--- goupup/src/test/scala/codes/cesartest.scala
+++ goupup/src/test/scala/codes/cesartest.scala
@@ -1,10 +1,34 @@
 import org.scalatest.FunSuite
 
 class CesarTest extends FunSuite {
+  val c = new cesar.Cesar(88)
+
   test("check cesar code") {
-    val c = new cesar.Cesar("tomasz", 10)
-    val afterCode = c.encode
+    val afterCode = c.encode("abcdfghijklmnopqwrz")
     val afterENcode = c.decode(afterCode)
-    assert(c.fraze == afterENcode)
+    assert( "abcdfghijklmnopqwrz" == afterENcode)
+  }
+
+  test("check set code char") {
+    val ch = c.setCode('~', 139)
+    assert(ch == 42)
+  }
+
+  test("check set code char2") {
+    val ch1 = c.setCode('a', 72)
+    println("a + 72: " + ch1)
+    val res = c.getCharCode(ch1.toChar,72)
+    assert(res == 97)
+  }
+
+  test("check get car code") {
+    val ch = c.getCharCode('!', 10)
+    println('!'.toInt - 10)
+    assert(ch == 119)
+  }
+
+  test("check get char3") {
+    val ch = c.getCharCode('!', 34)
+    assert(ch == 95)
   }
 }
