097bdeee14509312851aa3cf54a70fc630203a40
zsamotw <donkemot@interia.pl>
2018-01-16 14:06:48 +0100

    2018016

--- goupup/src/main/scala/s99/Trees.scala
+++ goupup/src/main/scala/s99/Trees.scala
@@ -14,35 +14,41 @@ object Trees {
         case i if i >= 2 => Node(value, balanced(n - 2, value), balanced(n - 2, value))
       }
     }
-  }
 
-  //56
-  def isSymetric[T](tree: Tree[T]) = {
-    def isMirror(tree1: Tree[T], tree2: Tree[T]): Boolean = (tree1, tree2) match {
-      case (End, End) => true
-      case (node1: Node[T], node2: Node[T]) => isMirror(node1.left, node2.left) && isMirror(node1.right, node2.right)
-      case _ => false
+    //56
+    def isSymetric[T] = {
+      def isMirror(tree1: Tree[T], tree2: Tree[T]): Boolean = (tree1, tree2) match {
+        case (End, End) => true
+        case (node1: Node[T], node2: Node[T]) => isMirror(node1.left, node2.left) && isMirror(node1.right, node2.right)
+        case _ => false
+      }
+      this match {
+        case End => false
+        case Node(_: T, left: Tree[T], right: Tree[T]) => isMirror(left, right)
+      }
     }
-    tree match {
-      case End => false
-      case Node(_: T, left: Tree[T], right: Tree[T]) => isMirror(left, right)
-    }
-  }
 
-  //57
-  def addValue[U >: T <% Ordered[T]](value: U, tree: Tree[U]) = {
+    //57
+    def addValue[B >: T](value: B)(implicit ordering: Ordering[B]): Tree[B]
+
 
   }
 
   case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree {
     override def toString = s"{$value | ${left.toString} + ${right.toString}}"
+
+    //57
+    override def addValue[B >: T](value: B)(implicit ordering: Ordering[B]): Tree[B] = this.value match {
+      case v if v > value => Node(v, this.left.addValue(value), this.right)
+      case v if v < value => Node(v, this.left, this.right.addValue(value))
+      case _ => this
+    }
   }
 
   case object End extends Tree[Nothing] {
     override def toString = "."
-  }
-  
-  object Node {
-    def apply[T](value: T): Node[T] = Node(value, End, End)
+
+    //57
+    override def addValue[B >: Nothing](value: B): Tree[B] = Node(value, End, End)
   }
 }
